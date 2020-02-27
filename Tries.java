f9ce20b603526ebd436aa7b2c409afe95ea74ebc
Aishwarya2710 <44317084+Aishwarya2710@users.noreply.github.com>
2020-02-27 19:59:35 +0530

    Add files via upload

---
+++ Tries.java
@@ -0,0 +1,43 @@
+public class Trie
+{
+    char c;
+    Trie[] children;
+    boolean word_exists;
+    Trie()
+    {
+        this.c = 0;
+        this.children = new Trie[26];
+        this.word_exists = false;
+    }
+    public void insert(String s)
+    {
+        if(s.length()== 0)
+            this.word_exists = true;
+        else
+        {
+            char letter = s.charAt(0);
+            if(this.children[letter-'a'] == null)
+                this.children[letter-'a'] = new Trie();
+            this.children[letter-'a'].insert(s.substring(1));
+        }
+    }
+    public boolean isWord(String s)
+    {
+        if(s.length() == 0)
+            return this.word_exists;
+        char letter = s.charAt(0);
+        if(this.children[letter-'a'] == null)
+            return false;
+        return this.children[letter-'a'].isWord(s.substring(1));
+    }
+    public static void main(String[] args)
+    {
+        Trie root = new Trie();
+        root.insert("hey");
+        root.insert("hello");
+        root.insert("try");
+        root.insert("tries");
+        boolean res = root.isWord("tries");
+        System.out.println(res);
+    }
+}
